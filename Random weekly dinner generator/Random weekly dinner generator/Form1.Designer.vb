024502700b821061e8628b70deffd945cd257389
Nick Sonnenberg <53633706+njsonnen@users.noreply.github.com>
2019-08-09 13:55:36 -0400

    Add files via upload

---
+++ Random weekly dinner generator/Random weekly dinner generator/Form1.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.lstRecipes = New System.Windows.Forms.ListBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnEdit = New System.Windows.Forms.Button()
+        Me.btnCreate = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(181, 259)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(816, 430)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(189, 269)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(804, 418)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Label1"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackColor = System.Drawing.Color.Salmon
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox2.Location = New System.Drawing.Point(347, 14)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(430, 193)
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'lstRecipes
+        '
+        Me.lstRecipes.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.lstRecipes.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lstRecipes.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstRecipes.FormattingEnabled = True
+        Me.lstRecipes.ItemHeight = 22
+        Me.lstRecipes.Location = New System.Drawing.Point(356, 22)
+        Me.lstRecipes.Name = "lstRecipes"
+        Me.lstRecipes.Size = New System.Drawing.Size(412, 178)
+        Me.lstRecipes.TabIndex = 5
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BackColor = System.Drawing.Color.GreenYellow
+        Me.PictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox3.Location = New System.Drawing.Point(171, 250)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(839, 452)
+        Me.PictureBox3.TabIndex = 6
+        Me.PictureBox3.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(225, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(116, 139)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Select a meal to cook:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(165, 215)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(168, 32)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Instructions:"
+        '
+        'btnEdit
+        '
+        Me.btnEdit.Location = New System.Drawing.Point(787, 31)
+        Me.btnEdit.Name = "btnEdit"
+        Me.btnEdit.Size = New System.Drawing.Size(103, 71)
+        Me.btnEdit.TabIndex = 9
+        Me.btnEdit.Text = "Edit Selected Meal"
+        Me.btnEdit.UseVisualStyleBackColor = True
+        '
+        'btnCreate
+        '
+        Me.btnCreate.Location = New System.Drawing.Point(787, 137)
+        Me.btnCreate.Name = "btnCreate"
+        Me.btnCreate.Size = New System.Drawing.Size(103, 61)
+        Me.btnCreate.TabIndex = 10
+        Me.btnCreate.Text = "Create a New Meal"
+        Me.btnCreate.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Random_weekly_dinner_generator.My.Resources.Resources.cooking_wallpaper
+        Me.ClientSize = New System.Drawing.Size(1148, 768)
+        Me.Controls.Add(Me.btnCreate)
+        Me.Controls.Add(Me.btnEdit)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lstRecipes)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Meal Database"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents lstRecipes As ListBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents btnEdit As Button
+    Friend WithEvents btnCreate As Button
+End Class
---
+++ Random weekly dinner generator/Random weekly dinner generator/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Dim recipeNametxt As String = ""
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Label1.Visible = False
+
+        lstRecipes.Items.Add("Terriakyi Chicken wcaliflower rice and vegetables")
+        lstRecipes.Items.Add("Burgers")
+        lstRecipes.Items.Add("Spagetti")
+        lstRecipes.Items.Add("Cranberry Chicken")
+        lstRecipes.Items.Add("Tacos")
+
+    End Sub
+
+    Private Sub LstRecipes_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstRecipes.SelectedIndexChanged
+
+        Select Case lstRecipes.SelectedIndex
+            Case 0
+                recipeNametxt = "Terriakyi Chicken wcaliflower rice and vegetables"
+            Case 1
+                recipeNametxt = "Burgers"
+            Case 2
+                recipeNametxt = "Spagetti"
+            Case 3
+                recipeNametxt = "Cranberry Chicken"
+            Case 4
+                recipeNametxt = "Tacos"
+        End Select
+
+        Label1.Visible = True
+        Label1.Text = My.Computer.FileSystem.ReadAllText("C:\Recipes\" & recipeNametxt & ".txt")
+
+    End Sub
+
+    Private Sub BtnCreate_Click(sender As Object, e As EventArgs) Handles btnCreate.Click
+
+
+
+    End Sub
+
+    Private Sub BtnEdit_Click(sender As Object, e As EventArgs) Handles btnEdit.Click
+
+
+
+    End Sub
+End Class
